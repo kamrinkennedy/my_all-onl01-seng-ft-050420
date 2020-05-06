096e23d428f2d60b73334dfa83d7c80ec7f61870
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-06 20:03:19 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,14 @@ require 'pry'
 
 def my_all?(collection)
   i = 0 
+  return_values = []
   while i < collection.length
-    yield(collection[i])
+    return_values << yield(collection[i])
     i += 1 
+  end
+  if return_values.include?(false)
+    false
+  else
+    true 
   end
 end
